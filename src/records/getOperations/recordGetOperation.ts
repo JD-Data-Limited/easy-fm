de1bc20be55717b2e8124de2cdb5022307d38d90
betabites <BetaBit3s@gmail.com>
2024-01-11 17:52:39 +1300

    Merged find.ts and recordGetOperation.ts into a single class for simplicity

--- src/records/getOperations/recordGetOperation.ts
+++ src/records/getOperations/recordGetOperation.ts
@@ -1,54 +1,121 @@
 /*
- * Copyright (c) 2023. See LICENSE file for more information
+ * Copyright (c) 2023-2024. See LICENSE file for more information
  */
 
 import {LayoutInterface} from "../../layouts/layoutInterface.js";
 import {Portal} from "../portal.js";
-import {limitPortalsInterface} from "../../types.js";
+import {portalFetchData, ScriptRequestData} from "../../types.js";
 import {LayoutBase} from "../../layouts/layoutBase.js"
+import {LayoutRecord} from "../layoutRecord";
+import {ApiRecordResponseObj} from "../../models/apiResults";
+import {FMError} from "../../FMError";
+
+export type SortOrder = "ascend" | "descend"
+export type FindRequest = {
+    [key: string]: string,
+    omit?: "true" | "false"
+}
 
 export class RecordGetOperation<T extends LayoutInterface> {
     protected layout: LayoutBase
-    protected limit: number = 100
-    protected scripts: object
-    protected sort: object[]
-    protected limitPortals: limitPortalsInterface[] = []
-    protected offset: number = 0
+    protected _limit: number = 100
+    protected scriptData: ScriptRequestData = {}
+    protected sortData: {fieldName: string, sortOrder: SortOrder}[] = []
+    protected portals: portalFetchData[] = []
+    protected _offset: number = 0
+    protected queries: FindRequest[] = []
 
     constructor(layout: LayoutBase) {
         this.layout = layout
-        this.scripts = {
-            "script": null, // Runs after everything
-            "script.prerequeset": null, // Runs before the request
-            "script.presort": null // Runs before the sort
+        this.sortData = []
+    }
+
+    get isFindRequest() {
+        return this.queries.length !== 0
+    }
+
+    protected generateParams() {
+        const params = {
+            _limit: this._limit.toString(),
+            _offset: this._offset.toString(),
+            _sort: JSON.stringify(this.sortData)
         }
-        this.sort = []
+        if (this.scriptData.after) params["script"] = this.scriptData.after.name
+        if (this.scriptData.after.parameter) params["script.param"] = this.scriptData.after.parameter
+
+        if (this.scriptData.presort) params["script.presort"] = this.scriptData.after.name
+        if (this.scriptData.presort.parameter) params["script.presort.param"] = this.scriptData.after.parameter
+
+        if (this.scriptData.prerequest) params["script.prerequest"] = this.scriptData.after.name
+        if (this.scriptData.prerequest.parameter) params["script.prerequest.param"] = this.scriptData.after.parameter
+
+        if (this.queries.length !== 0) params["query"] = this.queries
+
+        return params
+    }
+
+    scripts(scripts: ScriptRequestData) {
+        this.scriptData = scripts;
+        return this
     }
 
     /*
-    addToPortalLimit() will adjust the results of the get request so that if a layout has multiple portals in it,
-    only data from the specified ones will be read. This may help reduce load on your FileMaker API
+    portal() will adjust the results of the get request so that if a layout has multiple portals in it,
+    only data from the specified ones will be read. This may help reduce load on your FileMaker API.
+
+    By default, easy-fm will NOT fetch any portal data
     */
-    addToPortalLimit(portal: Portal<any>, offset = 0, limit = 100) {
+    portal(portalName: string, offset = 0, limit = 100) {
         if (offset < 0) throw "Portal offset cannot be less than 0"
-        this.limitPortals.push({portalName: portal.name, offset, limit})
+        this.portals.push({portalName: portalName, offset, limit})
         return this
     }
 
-    setLimit(limit: number) {
-        if (limit < 1) throw "Record limit too low"
-        this.limit = limit
+    sort(fieldName: string, sortOrder: SortOrder) {
+        this.sortData.push({fieldName, sortOrder})
         return this
     }
 
-    addSort(fieldName, sortOrder) {
-        this.sort.push({fieldName, sortOrder})
+    offset(offset: number) {
+        this._offset = offset
         return this
     }
 
-    setOffset(offset: number) {
-        if (offset < 0) throw "Record offset too low"
-        this.offset = offset
+    limit(limit: number) {
+        this._limit = limit
         return this
+    }
+
+    find(query: FindRequest) {
+        this.queries.push(query)
+        return this
+    }
+
+    async fetch(): Promise<LayoutRecord<T["fields"], T["portals"]>[]> {
+        let trace = new Error()
+        await this.layout.getLayoutMeta()
+
+        const is_find = this.isFindRequest
+        const endpoint = this.layout.endpoint + (is_find ? "/_find" : "/records")
+        const reqData = {
+            // port: 443,
+            method: is_find ? "POST" : "GET",
+            body: is_find ? JSON.stringify(this.generateParams()) : undefined,
+        }
+
+        let res = await this.layout.database.apiRequest<ApiRecordResponseObj>(
+            endpoint,
+            reqData
+        )
+        if (res.messages[0].code === "0") {
+            // console.log("RESOLVING")
+            if (!this.layout.metadata) await this.layout.getLayoutMeta()
+            return res.response.data.map(item => {
+                return new LayoutRecord(this.layout, item.recordId, item.modId, item.fieldData, item.portalData)
+            })
+        }
+        else {
+            throw new FMError(res.messages[0].code, res.httpStatus, res, trace)
+        }
     }
 }
