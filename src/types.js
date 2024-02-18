795e964432dfdbdaf307069ab6f5a4675bf002d9
betabites <BetaBit3s@gmail.com>
2024-02-19 12:04:50 +1300

    Adjusted code so that omit find requests work as expected in typescript

--- src/types.js
+++ src/types.js
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2022-2024. See LICENSE file for more information
  */
-export var DOWNLOAD_MODES;
-(function (DOWNLOAD_MODES) {
-    DOWNLOAD_MODES[DOWNLOAD_MODES["Stream"] = 0] = "Stream";
-    DOWNLOAD_MODES[DOWNLOAD_MODES["Buffer"] = 1] = "Buffer";
-})(DOWNLOAD_MODES || (DOWNLOAD_MODES = {}));
+export var DownloadModes;
+(function (DownloadModes) {
+    DownloadModes[DownloadModes["Stream"] = 0] = "Stream";
+    DownloadModes[DownloadModes["Buffer"] = 1] = "Buffer";
+})(DownloadModes || (DownloadModes = {}));
 export var RecordTypes;
 (function (RecordTypes) {
     RecordTypes[RecordTypes["UNKNOWN"] = 0] = "UNKNOWN";
