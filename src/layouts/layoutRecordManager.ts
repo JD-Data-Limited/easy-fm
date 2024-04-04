653d0c69ab9cf3e897638fe5ab56c264c17d421f
betabites <BetaBit3s@gmail.com>
2024-04-04 16:00:01 +1300

    Fixed layoutRecordManager.ts

--- src/layouts/layoutRecordManager.ts
+++ src/layouts/layoutRecordManager.ts
@@ -2,37 +2,37 @@
  * Copyright (c) 2023-2024. See LICENSE file for more information
  */
 
-import {LayoutRecord} from "../records/layoutRecord.js";
-import {LayoutInterface} from "./layoutInterface.js";
-import {LayoutBase} from "./layoutBase.js"
-import {GetOperationOptions, RecordGetOperation} from "../records/getOperations/recordGetOperation.js";
-import {PickPortals, RecordFetchOptions} from "../types.js";
-import {ApiFieldData} from "../models/apiResults.js";
+import {LayoutRecord} from '../records/layoutRecord.js'
+import {type LayoutInterface} from './layoutInterface.js'
+import {type LayoutBase} from './layoutBase.js'
+import {type GetOperationOptions, RecordGetOperation} from '../records/getOperations/recordGetOperation.js'
+import {type PickPortals, type RecordFetchOptions} from '../types.js'
+import {type ApiFieldData} from '../models/apiResults.js'
 
 export class LayoutRecordManager<T extends LayoutInterface> {
-    readonly layout: LayoutBase;
-    constructor(layout: LayoutBase) {
+    readonly layout: LayoutBase
+    constructor (layout: LayoutBase) {
         this.layout = layout
     }
 
     async create<OPTIONS extends RecordFetchOptions>(options: OPTIONS): Promise<LayoutRecord<
-        PickPortals<T, OPTIONS["portals"][number]>
+    PickPortals<T, OPTIONS['portals'][number]>
     >> {
-        let metadata = await this.layout.getLayoutMeta()
-        let fields: ApiFieldData = {}
-        for (let _field of metadata.fieldMetaData) {
-            fields[_field.name] = ""
+        const metadata = await this.layout.getLayoutMeta()
+        const fields: ApiFieldData = {}
+        for (const _field of metadata.fieldMetaData) {
+            fields[_field.name] = ''
         }
-        let portals: {[key: string]: []} = {}
-        for (let _portal of Object.keys(metadata.portalMetaData)) portals[_portal] = []
+        const portals: Record<string, []> = {}
+        for (const _portal of Object.keys(metadata.portalMetaData)) portals[_portal] = []
         return new LayoutRecord(this.layout, -1, 0, fields, portals)
     }
 
-    async get(recordId: number): Promise<LayoutRecord<
-        PickPortals<T, never>
+    async get (recordId: number): Promise<LayoutRecord<
+    PickPortals<T, never>
     >> {
         await this.layout.getLayoutMeta()
-        let record = new LayoutRecord<PickPortals<T, never>>(this.layout, recordId)
+        const record = new LayoutRecord<PickPortals<T, never>>(this.layout, recordId)
         await record.get()
         return record
     }
