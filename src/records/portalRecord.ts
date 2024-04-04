7262a97bb8eb7611d77df82cf7257d329999e3bf
betabites <BetaBit3s@gmail.com>
2024-04-04 16:01:16 +1300

    Fixed portalRecord.ts

--- src/records/portalRecord.ts
+++ src/records/portalRecord.ts
@@ -2,40 +2,40 @@
  * Copyright (c) 2023-2024. See LICENSE file for more information
  */
 
-import {RecordBase} from "./recordBase.js";
-import {extraBodyOptions, RecordTypes} from "../types.js";
+import {RecordBase} from './recordBase.js'
+import {type extraBodyOptions, RecordTypes} from '../types.js'
 
-import {RecordFieldsMap} from "../layouts/recordFieldsMap.js";
-import {PortalBase} from "./portalBase.js";
-import {Field, FieldValue} from "./field.js";
+import {type RecordFieldsMap} from '../layouts/recordFieldsMap.js'
+import {type PortalBase} from './portalBase.js'
+import {type Field, type FieldValue} from './field.js'
 
 export class PortalRecord<T extends RecordFieldsMap> extends RecordBase<T> {
-    readonly portal: PortalBase<T>;
+    readonly portal: PortalBase<T>
     readonly type = RecordTypes.PORTAL
 
-    constructor(record: RecordBase<any>, portal: PortalBase<any>, recordId: number, modId = recordId, fieldData = {}) {
-        super(record.layout, recordId, modId, fieldData);
+    constructor (record: RecordBase<any>, portal: PortalBase<any>, recordId: number, modId = recordId, fieldData = {}) {
+        super(record.layout, recordId, modId, fieldData)
         this.portal = portal
     }
 
-    _onSave() {
-        super._onSave();
+    _onSave () {
+        super._onSave()
         this.portal.record._onSave()
     }
 
-    commit(extraBody: extraBodyOptions = {}) {
-        return this.portal.record.commit(extraBody)
+    async commit (extraBody: extraBodyOptions = {}) {
+        return await this.portal.record.commit(extraBody)
     }
 
-    toObject(fieldFilter: (a: Field<FieldValue>) => boolean): {
-        modId?: string,
-        recordId?: string,
-    } & {[key: string]: string} {
-        let res: any = {
+    toObject (fieldFilter: (a: Field<FieldValue>) => boolean): {
+        modId?: string
+        recordId?: string
+    } & Record<string, string> {
+        const res: any = {
             recordId: this.recordId === -1 ? undefined : this.recordId.toString(),
             modId: this.modId === -1 ? undefined : this.modId.toString()
         }
-        for (let field of this.fieldsArray.filter(a => fieldFilter(a))) res[field.id] = field.value?.toString()
+        for (const field of this.fieldsArray.filter(a => fieldFilter(a))) res[field.id] = field.value?.toString()
         // console.log(res)
         return res
     }
