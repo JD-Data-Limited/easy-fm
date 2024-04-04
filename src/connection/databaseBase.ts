5fcb19fc9b4823b2e52d5d23309f767441b641cf
betabites <BetaBit3s@gmail.com>
2024-04-04 15:37:26 +1300

    Fixed databaseBase.ts

--- src/connection/databaseBase.ts
+++ src/connection/databaseBase.ts
@@ -2,18 +2,18 @@
  * Copyright (c) 2023-2024. See LICENSE file for more information
  */
 
-import {HostBase} from "./HostBase.js"
-import {ApiResults} from "../models/apiResults.js";
-import {IncomingMessage} from "http";
+import {type HostBase} from './HostBase.js'
+import {type ApiResults} from '../models/apiResults.js'
+import {type IncomingMessage} from 'http'
 
 export interface DatabaseBase {
-    host: HostBase,
+    host: HostBase
     readonly name: string
     endpoint: string
 
     // layouts: DatabaseStructure["layouts"]
 
-    apiRequestJSON<T = unknown>(url: string | Request, options?: RequestInit | undefined, autoRelogin?: boolean): Promise<ApiResults<T>>
-    apiRequestRaw(url: string | Request, options?: RequestInit | undefined): Promise<Response>
-    streamURL(url: string): Promise<IncomingMessage>
+    apiRequestJSON: <T = unknown>(url: string | Request, options?: RequestInit | undefined, autoRelogin?: boolean) => Promise<ApiResults<T>>
+    apiRequestRaw: (url: string | Request, options?: RequestInit | undefined) => Promise<Response>
+    streamURL: (url: string) => Promise<IncomingMessage>
 }
