77d3a2a045af16bdff091e449addcde3c82f4e77
betabites <BetaBit3s@gmail.com>
2024-04-04 15:56:41 +1300

    Fixed HostBase.ts

--- src/connection/HostBase.ts
+++ src/connection/HostBase.ts
@@ -2,15 +2,15 @@
  * Copyright (c) 2023-2024. See LICENSE file for more information
  */
 
-import {FMHostMetadata} from "../types.js";
-import {Moment} from "moment";
+import {type FMHostMetadata} from '../types.js'
+import {type Moment} from 'moment'
 
 export interface HostBase {
     readonly hostname: string
-    readonly timezoneOffsetFunc: (moment: Moment) => number,
+    readonly timezoneOffsetFunc: (moment: Moment) => number
     readonly verify: boolean
     metadata: FMHostMetadata
-    getMetadata(): PromiseLike<any>
+    getMetadata: () => PromiseLike<any>
     timeFormat: string
     dateFormat: string
     timeStampFormat: string
