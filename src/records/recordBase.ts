b62ba95be89888489042675e9b58572afad6b3a4
betabites <BetaBit3s@gmail.com>
2024-04-04 16:01:43 +1300

    Fixed recordBase.ts

--- src/records/recordBase.ts
+++ src/records/recordBase.ts
@@ -2,70 +2,67 @@
  * Copyright (c) 2023-2024. See LICENSE file for more information
  */
 
-import {EventEmitter} from "events";
-import * as moment from "moment";
-import {Moment} from "moment";
-import {RecordTypes} from "../types.js";
-import {RecordFieldsMap} from "../layouts/recordFieldsMap.js";
-import {LayoutBase} from "../layouts/layoutBase.js";
-import {ApiFieldData} from "../models/apiResults.js";
-import {Field, FieldValue} from "./field.js";
+import {EventEmitter} from 'events'
+import * as moment from 'moment'
+import {type Moment} from 'moment'
+import {RecordTypes} from '../types.js'
+import {type RecordFieldsMap} from '../layouts/recordFieldsMap.js'
+import {type LayoutBase} from '../layouts/layoutBase.js'
+import {type ApiFieldData} from '../models/apiResults.js'
+import {Field, type FieldValue} from './field.js'
 
 export abstract class RecordBase<T extends RecordFieldsMap> extends EventEmitter {
-    readonly layout: LayoutBase;
-    readonly type: RecordTypes = RecordTypes.UNKNOWN;
-    public recordId: number;
-    modId: number;
-    fields: T;
-    protected portalData: any[] = [];
+    readonly layout: LayoutBase
+    readonly type: RecordTypes = RecordTypes.UNKNOWN
+    public recordId: number
+    modId: number
+    fields: T
+    protected portalData: any[] = []
 
-    protected constructor(layout: LayoutBase, recordId: number, modId = recordId, fieldData: ApiFieldData) {
-        super();
+    protected constructor (layout: LayoutBase, recordId: number, modId = recordId, fieldData: ApiFieldData) {
+        super()
         this.layout = layout
         this.recordId = recordId
         this.modId = modId
         this.fields = this.processFieldData(fieldData)
     }
 
-    get endpoint(): string {
+    get endpoint (): string {
         return `${this.layout.endpoint}/records/${this.recordId}`
     }
 
-    get edited(): boolean {
+    get edited (): boolean {
         return !!this.fieldsArray.find(i => i.edited)
     }
 
-    get fieldsArray(): Field<FieldValue>[] {
+    get fieldsArray (): Array<Field<FieldValue>> {
         return Object.values(this.fields)
     }
 
-    protected processFieldData(fieldData: ApiFieldData) {
-        let fields: RecordFieldsMap = {}
+    protected processFieldData (fieldData: ApiFieldData) {
+        const fields: RecordFieldsMap = {}
 
-        for (let key of Object.keys(fieldData)) {
-            let _field = new Field<number | string | Moment>(this, key, fieldData[key])
-            if (!!fieldData[key]) {
-                if (_field.metadata.result === "timeStamp") {
-                    // @ts-ignore
+        for (const key of Object.keys(fieldData)) {
+            const _field = new Field<number | string | Moment>(this, key, fieldData[key])
+            if (fieldData[key]) {
+                if (_field.metadata.result === 'timeStamp') {
+                    // @ts-expect-error
                     let date = moment.default(fieldData[key])
                     date = date
                         .utcOffset(this.layout.database.host.timezoneOffsetFunc(date), true)
                         .local()
                     _field.set(date)
                     _field.edited = false
-
-                }
-                else if (_field.metadata.result === "time") {
-                    // @ts-ignore
+                } else if (_field.metadata.result === 'time') {
+                    // @ts-expect-error
                     let date = moment.default(fieldData[key])
                     date = date
                         .utcOffset(this.layout.database.host.timezoneOffsetFunc(date), true)
                         .local()
                     _field.set(date)
                     _field.edited = false
-                }
-                else if (_field.metadata.result === "date") {
-                    // @ts-ignore
+                } else if (_field.metadata.result === 'date') {
+                    // @ts-expect-error
                     let date = moment.default(fieldData[key])
                     date = date
                         .utcOffset(this.layout.database.host.timezoneOffsetFunc(date), true)
@@ -80,8 +77,8 @@ export abstract class RecordBase<T extends RecordFieldsMap> extends EventEmitter
         return fields as T
     }
 
-    _onSave() {
-        this.emit("saved")
-        for (let field of this.fieldsArray) field.edited = false
+    _onSave () {
+        this.emit('saved')
+        for (const field of this.fieldsArray) field.edited = false
     }
 }
